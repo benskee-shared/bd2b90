f1fe0aaaf547779980f6ec6d70a5d3b166ac35e6
benskee <benskee33@gmail.com>
2022-04-11 14:41:02 -0400

    Fixed bug 1. New messages added to conversation and in proper order.

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,9 +62,9 @@ const Home = ({ user, logout }) => {
     });
   };
 
-  const postMessage = (body) => {
+  const postMessage = async (body) => {
     try {
-      const data = saveMessage(body);
+      const data = await saveMessage(body);
 
       if (!body.conversationId) {
         addNewConvo(body.recipientId, data.message);
@@ -80,17 +80,20 @@ const Home = ({ user, logout }) => {
 
   const addNewConvo = useCallback(
     (recipientId, message) => {
-      conversations.forEach((convo) => {
+      const updatedConversations = [...conversations]
+
+      updatedConversations.forEach((convo) => {
         if (convo.otherUser.id === recipientId) {
           convo.messages.push(message);
           convo.latestMessageText = message.text;
           convo.id = message.conversationId;
         }
       });
-      setConversations(conversations);
+      setConversations(updatedConversations);
     },
     [setConversations, conversations],
   );
+
   const addMessageToConversation = useCallback(
     (data) => {
       // if sender isn't null, that means the message needs to be put in a brand new convo
@@ -104,14 +107,18 @@ const Home = ({ user, logout }) => {
         newConvo.latestMessageText = message.text;
         setConversations((prev) => [newConvo, ...prev]);
       }
+      
+      const updatedConversations = [...conversations]
 
-      conversations.forEach((convo) => {
+      updatedConversations.forEach((convo) => {
         if (convo.id === message.conversationId) {
-          convo.messages.push(message);
+          convo.messages = [message, ...convo.messages];
           convo.latestMessageText = message.text;
         }
       });
-      setConversations(conversations);
+
+      setConversations(updatedConversations);
+
     },
     [setConversations, conversations],
   );
